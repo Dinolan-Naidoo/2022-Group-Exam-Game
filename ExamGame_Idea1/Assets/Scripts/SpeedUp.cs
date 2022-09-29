bfc3360d2cb0d44120023bae92786c0d17780756
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-09-29 20:11:47 +0200

    speed up bocks and second level start

--- ExamGame_Idea1/Assets/Scripts/SpeedUp.cs
+++ ExamGame_Idea1/Assets/Scripts/SpeedUp.cs
@@ -12,31 +12,41 @@ public class SpeedUp : MonoBehaviour
         
     }
 
-    // Update is called once per frame
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.tag == "Player")
+        if (collision.gameObject.CompareTag("Player"))
         {
+            Debug.Log("player must speed up");
+            StartCoroutine(PickUp(collision));
 
+        }
+    }
+    /*private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("player must speed up");
            StartCoroutine (PickUp(collision));
 
         }
 
-    }
+    }*/
 
-    IEnumerator PickUp(Collider2D player)
+    IEnumerator PickUp(Collision2D player)
     {
 
 
-        Player_Controller speed_ =player.GetComponent<Player_Controller>();
+        Player_Controller speed_ =player.gameObject.GetComponent<Player_Controller>();
         speed_.speed += 15f;
+
+
 
 
         yield return new WaitForSeconds(2f);
 
         speed_.speed -= 15f;
 
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 
 
