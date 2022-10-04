d48323d48687d9c4068820d90a618bcc5f0439d4
raramai-coder <71047546+raramai-coder@users.noreply.github.com>
2022-10-04 09:14:14 +0200

    track player position in race

--- ExamGame_Idea1/Assets/Scripts/GameManager.cs
+++ ExamGame_Idea1/Assets/Scripts/GameManager.cs
@@ -2,20 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject currentPlayer;
+    [SerializeField] private GameObject otherPlayer;
+    [SerializeField] private TextMeshProUGUI positionText;
 
-    /*// Update is called once per frame
+    /* // Start is called before the first frame update
+     void Start()
+     {
+
+     }*/
+
+    // Update is called once per frame
     void Update()
     {
-        
-    }*/
+        CheckPosition();
+    }
+    
+    private void CheckPosition()
+    {
+        if (currentPlayer.transform.position.x>otherPlayer.transform.position.x)
+        {
+            //Debug.Log("player 1 in lead");
+            positionText.text = "Position: 1/2";
+        }
+        else
+        {
+            //Debug.Log("player 2 in lead");
+            positionText.text = "Position: 2/2";
+        }
+    }
 
     public void Exit()
     {
